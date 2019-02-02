540768fa9ec9dfa1b1e387e35cff972195caa20c
H B <phajib@gmail.com>
2019-02-02 09:44:22 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,6 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     user_input = gets.chomp
     number = rand(1..6)
-    #case user_input
     if user_input == "exit"
       puts "Goodbye!"
       break
