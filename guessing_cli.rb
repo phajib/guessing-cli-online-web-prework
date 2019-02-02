c0423e521edccde02a45a6830ed4d6f596c6170f
H B <phajib@gmail.com>
2019-02-02 09:36:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,20 @@
-def Guessing CLI
-  
+def run_guessing_game
+  loop do
+    puts "Guess a number between 1 and 6."
+    user_input = gets.strip
+    number = rand(1..6)
+    case user_input
+    when "exit"
+      puts "Goodbye!"
+      break
+    when "#{user_input}" == number
+      puts "You guessed the correct number!"
+      break
+    when "#{user_input}" != number
+      puts "The computer guessed #{number}."
+      break
+    else
+      puts "Invalid input."
+    end
+  end
 end
